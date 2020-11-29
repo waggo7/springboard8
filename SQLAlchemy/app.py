14b26bf81d026bffa1be627e54aac672f333b752
waggo7 <63073660+waggo7@users.noreply.github.com>
2020-11-29 01:51:15 -0600

    Add files via upload

--- SQLAlchemy/app.py
+++ SQLAlchemy/app.py
@@ -1,9 +1,9 @@
 from flask import Flask,request,flash, session,render_template, redirect,url_for,jsonify
-from models import db, connect_db, User
+from models import db, connect_db, User, Post
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = False
 #app.config['SECRET_KEY'] = "chickenzarecool21837"
@@ -14,47 +14,117 @@ connect_db(app)
 db.create_all()
 
 @app.route('/')
-def list_users():
-    return redirect("/newuser")
+def land():
+    return render_template('landing.html')
 
-@app.route("/newuser")
+@app.errorhandler(404)
+def page_not_found(e):
+    """Show 404 NOT FOUND page."""
+
+    return render_template('404.html'), 404
+
+@app.route('/user')
+def user_home_page():
+    return render_template('user/index.html')
+
+@app.route('/user/newuser',  methods=["GET"])
+def new_user():
+    return render_template('user/newuser.html')#,  User=user)
+
+@app.route('/user/adduser', methods=["GET"])
 def login():
-    user = User.query.order_by(User.last_name,User.fullname).all()
-    # if user != None:
-    #     flash("this user exists, please try again")
-    return render_template("index.html", user=user)
+    user = User.query.order_by(User.last_name,User.first_name).all()
+    return render_template("user/index.html", User=user)
 
-@app.route("/checkuser", methods=["GET"])
-def add_user():
-    return render_template()
-
-@app.route("/checkuser", methods=["POST"])
+@app.route("/user/checkuser", methods=["GET"])
+def user_get_check():
+    return render_template('user/newuser.html')
+    
+@app.route("/user/checkuser", methods=["POST"])
 def user_check():
-
     new_user=User(
         first_name= request.form['first_name'],
         last_name= request.form['last_name'],
         image_url = request.form['image_url']or None)
-    
-    # if new_user.last_name == None or new_user.first_name==None:
-    #         flash("this user exists, please try again")
-    #         return render_template('index.html')
     db.session.add(new_user)
     db.session.commit()
-    
-    return redirect('/newuser')
+    return redirect('/user/adduser')#, user = new_user)
     #return render_template("userpage.html", User=new_user)
-@app.route("/idassignuser/<int:user_id>")
-def idassignuser(user_id):
+
+@app.route('/user/<int:user_id>')
+def users_show(user_id):
+
     user = User.query.get_or_404(user_id)
-    return render_template('userinfo.html', user=user)
+    return render_template('user/userpage.html', User=user)
+
+@app.route("/user/<int:user_id>/edit")
+def id_asign(user_id):
+    user = User.query.get_or_404(user_id)
+    return render_template('user/edit.html',  User= user)
 
 
+@app.route("/user/<int:user_id>/edit")
+def user_edit(user_id):
+    user = User.query.get_or_404(user_id)
+    user.first_name = request.form['first_name']
+    user.last_name = request.form['last_name']
+    user.image_url = request.form['image_url']
 
-# @app.route('/newuser', methods=["POST"])
-# def new_user(user_id):
-#     user_id = User.query.get_or_404(user_id)
-#     return render_template('useradd.html', user= user_id)
+    db.session.add(user)
+    db.session.commit()
+    return redirect('/user/adduser')
+
+@app.route("/user/<int:user_id>/delete", methods=["POST"])
+def delete(user_id):
+    user = User.query.get_or_404(user_id)
+    db.session.delete(user)
+    db.session.commit()
+    return redirect("/user/adduser")
+
+################################POSTS########################
+@app.route("/post/home")
+def show_post():
+
+    return render_template('post/home.html')
+
+@app.route("/user/<int:user_id>/post/new")
+def newuser_postid(user_id):
+    user = User.query.get_or_404(user_id)
+    return render_template('/post/index.html', User=user)
 
 
+@app.route("/user/<int:user_id>/posts/new", methods=["POST"])
+def newuser_post(user_id):
+    user = User.query.get_or_404(user_id)
+    new_post = Post(
+        title=request.form['title'],
+        content=request.form['content']
+            )
+    db.session.add(new_post)
+    db.session.commit()
+    return redirect(f'/user/<{user_id}')
 
+@app.route("/user/<int:post_id>")
+def showpost(post_id):
+    post = Post.query.get_or_404(post_id)
+    return render_template('post/home.html', post =post)
+        
+@app.route("/post/<int:post_id>/edit")
+def edit_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    return render_template('post/edit.html', post =post)
+
+@app.route("/post/<int:post_id>/edit", methods=["POST"])
+def edituser_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    post.title = request.form['title']
+    post.content = request.form['content']
+    return redirect('/post/<int:post_id>', post=post)
+
+@app.route("/post/<int:post_id>/delete",methods=["POST"])
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    db.session.delete(post)
+    db.session.commit()
+    return redirect('post/<int:post_id>')
+        
--- SQLAlchemy/models.py
+++ SQLAlchemy/models.py
@@ -1,6 +1,7 @@
 """Models for Blogly."""
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql.schema import ForeignKey
 
 db = SQLAlchemy()
 
@@ -16,14 +17,31 @@ class User(db.Model):
     first_name = db.Column(db.String(20), nullable=False,unique=True)
     last_name =  db.Column(db.String(20), nullable=False,unique=True)
     image_url =  db.Column(db.Text,nullable=True)
+
+    Post = db.relationship("Post", backref="user", cascade="all, delete-orphan")
     
-    @classmethod
-    def fullname(cls):
-        """return user name"""
-        return f"{cls.last_name},{cls.first_name}"
+    @property
+    def fullname(self):
+
+        return f"{self.last_name},{self.first_name}"
     
 
+class Post(db.Model):
+    __tablename__ = "Post"
+    id= db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title=  db.Column(db.String, nullable=False)
+    content= db.Column(db.String,nullable=False)
+    created_at= db.Column(db.DateTime, nullable=False)
+    user_id = db.Column(db.ForeignKey("User.id"), nullable=False)
+    
+    @property
+    def friendly_date(self):
 
+        return self.created_at.strftime("%a %b %-d  %Y, %-I:%M %p")
+
+
+
+    
 
 
 
