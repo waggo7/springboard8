affbbc80bd9041aea9223856954a89667dc09ee0
waggo7 <63073660+waggo7@users.noreply.github.com>
2021-01-28 21:53:11 -0600

    Add files via upload

---
+++ lucky-nums2/static/lucky.js
@@ -0,0 +1,12 @@
+/** processForm: get data from form and make AJAX call to our API. */
+
+function processForm(evt) {
+}
+
+/** handleResponse: deal with response from our lucky-num API. */
+
+function handleResponse(resp) {
+}
+
+
+$("#lucky-form").on("submit", processForm);
