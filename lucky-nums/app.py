43fda7284474576a7eac66b4e4486b187368b683
waggo7 <63073660+waggo7@users.noreply.github.com>
2021-01-29 16:39:48 -0600

    Add files via upload

--- lucky-nums/app.py
+++ lucky-nums/app.py
@@ -1,10 +1,37 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, json, jsonify, redirect, session, g
+from flask_debugtoolbar import DebugToolbarExtension
+from form import UserForm
+from models import db, connect_db, User
+import json
+import requests
 
+response = requests.get('http://numbersapi.com/')
 app = Flask(__name__)
-
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///flask2'
+app.config['SECRET_KEY'] = "oh-so-secret"
+toolbar = DebugToolbarExtension(app)
 
 @app.route("/")
 def homepage():
     """Show homepage."""
-
+    
     return render_template("index.html")
+
+@app.route("/api/get-lucky-num", methods=["POST"])
+def makeapi():
+    form= UserForm()
+    print(form)
+    #button = 
+    if form.validate_on_submit():
+        name= form.data['name']
+        email= form.data['email']
+        year= form.data['year']
+        color= form.data['color']
+        newuser= User(name=name,email=email,year=year,color=color)
+        print(newuser)
+        db.session.add(newuser)
+        db.session.commit()
+        return jsonify(newuser)
+        
+    return render_template('index.html')
+ b 
--- lucky-nums/static/lucky.js
+++ lucky-nums/static/lucky.js
@@ -1,12 +1,38 @@
 /** processForm: get data from form and make AJAX call to our API. */
+const response = axios.get('https://numbersapi.com/')
 
 function processForm(evt) {
+    evt.preventDefault();
+    const $name = $('name');
+    const email = document.getElementById('email')
+    const color = document.getElementById('color')
+    console.log(name, name.data); //testing
+    $.ajax({
+        url: "/api/get-lucky-number",
+        data: {},
+        success: function(data) {
+            $.each(data, function(i, item) {
+                if (item == "num")
+                    return item;
+            })
+        },
+        error: function(data) {
+            $.each(data, function(i, item) {
+                return item + " is not valid please try again";
+            })
+
+        }
+    })
+
+    const newUser = new UserForm()
+    return;
 }
 
 /** handleResponse: deal with response from our lucky-num API. */
 
-function handleResponse(resp) {
+function handleResponse(resp) {}
+
+function hello() {
+    return console.log('hello world');
 }
-
-
 $("#lucky-form").on("submit", processForm);
